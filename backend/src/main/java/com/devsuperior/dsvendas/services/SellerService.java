f3718f5d5a9ec138e299f9570f7a2ff906a00a45
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-05-06 09:26:07 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsvendas.services;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
